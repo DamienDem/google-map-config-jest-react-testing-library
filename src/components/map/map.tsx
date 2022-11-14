308745007e09b70ab9a50e8aab4891b068c40096
ddemolin <damien.demolin@atos.net>
2022-11-14 15:53:06 +0100

    feat: add map

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,5 +1,14 @@
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 const Map = () => {
-    return <> map </>
-}
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_KEY as string,
+    libraries:["places"]
+  });
 
-export default Map
+  if(!isLoaded) return <div>Loading ...</div>
+  return (
+    <GoogleMap zoom={14} center={{lat:50.63473, lng:3.08220}} mapContainerClassName="h-screen"></GoogleMap>
+  );
+};
+
+export default Map;
